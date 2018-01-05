2ca1248394b3d7539eb602d3fe3cea73933bbc55
Paolo Pisati <paolo.pisati@canonical.com>
2018-01-05 23:28:41 +0100

    FTBFS: use gpio_chip.parent to store dev
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- gpio-nct5104d.c
+++ gpio-nct5104d.c
@@ -273,7 +273,7 @@ static int nct5104d_gpio_probe(struct platform_device *pdev)
 	for (i = 0; i < data->nr_bank; i++) {
 		struct nct5104d_gpio_bank *bank = &data->bank[i];
 
-		bank->chip.dev = &pdev->dev;
+		bank->chip.parent = &pdev->dev;
 		bank->data = data;
 
 		err = gpiochip_add(&bank->chip);
